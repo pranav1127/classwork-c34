40a178a22999a7537f8eca615bd4aae191b653dc
pranav1127 <74109716+pranav1127@users.noreply.github.com>
2021-01-30 11:17:38 -0500

    Add files via upload

---
+++ Rope.js
@@ -0,0 +1,40 @@
+class Rope{
+    constructor(bodyA,pointB){
+    var options={
+    bodyA:bodyA,
+    pointB:pointB,
+    stiffness:1.0,
+    length:450
+    }
+    this.pointB=pointB
+    this.rope=Constraint.create(options);
+    
+    World.add(world,this.rope);
+    
+    }
+    fly(){
+this.rope.bodyA=null;
+
+    }
+    display(){
+        if(this.rope.bodyA){
+            var pointA=this.rope.bodyA.position;
+            var pointB=this.pointB
+            push();
+           
+           
+            
+            fill("black");
+            strokeWeight(4);
+            line(pointB.x,pointB.y,pointA.x,pointA.y);
+            pop();
+        }
+    
+    }
+    
+    
+    
+    
+    
+    
+    }
